9ff46979ea0fbce1a51f0b783dd762b2a8a56243
azuressu <martina0130@naver.com>
2023-06-09 12:34:29 +0900

    add comment, update showCustomerMenu()

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+    // 크기, 숙박비
+    private String roomSize;
+    private Double roomCharge;
+    // 방마다 리스트를 만들어줌
+    private List<String> dateList = new ArrayList<>();
+
+
+    public String getRoomSize() {
+        return roomSize;
+    }
+
+    public Double getRoomCharge() {
+        return roomCharge;
+    }
+
+    public List<String> getDateList() {
+        return dateList;
+    }
+
+    public void setDateList(String date) {
+        this.dateList.add(date);
+    }
+
+    Room(String roomSize, Double roomCharge) {
+        this.roomSize = roomSize;
+        this.roomCharge = roomCharge;
+    }
+
 }
